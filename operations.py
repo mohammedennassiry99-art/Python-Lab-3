88a79994f53bfbab4d529754cf632671608faa71
Mohammed <mohammedennassiry99@gmail.com>
2025-12-04 22:22:37 +0100

    Lab 3

---
+++ operations.py
@@ -0,0 +1,15 @@
+a = 15
+b = 4
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+
+print(f"{a} // {b} = {a // b}")    
+print(f"{a} % {b} = {a % b}")     
+print(f"{a} ** {b} = {a ** b}")   
+
+print(a == b)   
+print(a != b)   
+print(a < b)   
+print(a >= b)   
